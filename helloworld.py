74d4b459569c24d6ed95958c881c362a7328875a
ramesh konatala <konatalaramesh@gmail.com>
2016-02-18 18:40:33 +0530

    Will be able to give steps

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
-alist=[1,2,3]
-blist=[1,2,3,4]
-print alist==blist
+
+k=[]
+k1=[item for item in range((15/2)+1)]
+k2=[(15-1-item) for item in range(15/2)]
+k.append(k1)
+k.append(k2)
+print k
--- ipopFinal.py
+++ ipopFinal.py
@@ -206,6 +206,7 @@ def arrangementWhole(clist):
 		            vals.append(clist[j][i])
 		return vals  
 	else:
+		clist=[inner for outer in clist for inner in outer]
 		return clist
 
 
@@ -316,9 +317,8 @@ for i in range(Number_of_sets):
 	Type_of_arrangement.append(arrangementInd(alist))
 	finalList.append(alist)
 	outputList=tuple(finalList)
+
 	
-
-
 finalList=arrangementWhole(finalList)
 for i in range(len(finalList)):
 	finalList[i]=str(finalList[i])
@@ -332,6 +332,7 @@ outputList=list(outputList)
 
 print "Final list is ",finalList
 print "Input List is ",inputList
+sortedList=[]
 
 def execute1():
 	i=0
@@ -349,10 +350,85 @@ def execute1():
 					sorted1=True
 				i+=1
 				k+=1
-				print inputList
+				sortedList.append(" ".join(inputList))
+				print " ".join(inputList)
 			else:
 				i+=1
 				
 		i=0	
 		j+=1
+
+		
+def execute2():
+		
+	k=[]
+	k.append([item for item in range(int((len(outputList[0]))))])
+	k.append([(len(finalList)-1-item) for item in range(int(len(outputList[1])))])
+	i=0
+	j=0
+	sorted1=False
+	while not sorted1:
+		while(i<len(outputList)and not sorted1):
+			if j<len(outputList[i]):
+				arrange(inputList,outputList[i][k[i][j]-len(outputList[0])],k[i][j])
+				i+=1
+				if (inputList==finalList):
+					sorted1=True
+				sortedList.append(" ".join(inputList))
+				print " ".join(inputList)
+			else:
+				i+=1
+		i=0
+		j+=1
+
+if Number_of_sets!=2:
+	execute1()
+else:
+	execute2()
+
+#print sortedList
+
+done=1
+while done!='0':
+	raw_input("Press enter to continue....")
+	print ("Enter type of the question from following list ")
+	question_type=raw_input("Type 1 : How many steps are required to sort it?\nType 2 : What is step X?\nType 3 : In which step do we get following order?\nType 4 : In step X, what is Yth element from left/right\nType 5 : what is position of element X in step Y\nType 6 : In step X what are the number of words between A and B?\nType 0 if you want to exit \n")
+	done=question_type
+	if question_type=='1':
+		print "Number of steps required to sort is : ",len(sortedList)
+	elif question_type=='2':
+		step=int(raw_input("Enter the step no. : "))
+		print "Step {} is : \n".format(step),sortedList[step-1]
+	elif question_type=='3':
+		order1=raw_input("Enter the order without any quotes ")
+		print "In step - {} we get above order : ".format(sortedList.index(order1)+1)
+	elif question_type=='4':
+		X=int(raw_input("Enter step no."))
+		Y=int(raw_input("Enter the position no. of the element wrt left/right "))
+		gh=raw_input("Postion is from left or right?(Enter all in small caps) ")
+		listelement=sortedList[X-1].split()
+		#print listelement
+		if gh=='left':
+			print "Element in step - {} at postion - {} from {} is : {}".format(X,Y,gh,listelement[Y-1])
+		else:
+			print "Element in step - {} at postion - {} from {} is : {}".format(X,Y,gh,listelement[len(listelement)-Y])
+	elif question_type=='5':
+		X=int(raw_input("Enter step no."))
+		gh=raw_input("What is the element?(Enter all in small caps")
+		listelement=sortedList[X-1].split()
+		for i in range(len(listelement)):
+			if listelement[i]==gh:
+				ph=i+1
+				break
+		print "Element is at position - {} in step - {}".format(ph,X)
+	elif question_type=='6':
+		X=int(raw_input("Enter step no."))
+		gh=raw_input("What is the first element?(Enter all in small caps)")
+		fh=raw_input("What is the second element?(Enter all in small caps)")
+		listelement=sortedList[X-1].split()
+		value=abs(listelement.index(gh)-listelement.index(fh))
+		print "There are {} words between {} and {} in step - {}".format(value,gh,fh,X)
+	else:
+		if question_type!='0':
+			print "error in input"
 		
