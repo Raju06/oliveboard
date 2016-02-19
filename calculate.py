23758afd8408eb196db4d1a034aeab37d9ebb3c5
ramesh konatala <konatalaramesh@gmail.com>
2016-02-19 16:58:20 +0530

    Program to solve linear euations for solving series type of questions'

--- calculate.py
+++ calculate.py
@@ -1,4 +1,5 @@
-from sympy import solve_poly_system
-from sympy.abc import x,y
-
-print solve_poly_system([x+y-81,1.2*x-1.5*y],x,y)
+import numpy as np
+a = np.array([[1,1,1,1],[8,4,2,1],[27,9,3,1],[64,16,4,1]])
+b = np.array([0,6,24,60])
+x = np.linalg.solve(a, b)
+print (x[0]*125)+(x[1]*25)+(x[2]*5)+(x[3]*1)
