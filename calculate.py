e8df6c85c4b1d9068280ed70d035316dcc39ee08
ramesh konatala <konatalaramesh@gmail.com>
2016-02-19 14:11:07 +0530

    solve system of equations

--- calculate.py
+++ calculate.py
@@ -1,4 +1,4 @@
-from sympy import sin, limit,solve
-from sympy import Symbol
-x = Symbol('x')
-print solve(x**2 - 1-x, x)
+from sympy import solve_poly_system
+from sympy.abc import x,y
+
+print solve_poly_system([x+y-81,1.2*x-1.5*y],x,y)
