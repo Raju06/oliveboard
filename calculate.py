c7aca4892a0d312fe4542ffd1c67ad94a72ed6ea
ramesh konatala <konatalaramesh@gmail.com>
2016-02-22 17:11:10 +0530

    scraping file added

--- calculate.py
+++ calculate.py
@@ -1,5 +1,1 @@
-import numpy as np
-a = np.array([[1,1,1,1],[8,4,2,1],[27,9,3,1],[64,16,4,1]])
-b = np.array([0,6,24,60])
-x = np.linalg.solve(a, b)
-print (x[0]*125)+(x[1]*25)+(x[2]*5)+(x[3]*1)
+print 15*11*13
