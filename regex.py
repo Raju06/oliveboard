06d5dc826fa369d7303c5238bdddc8ac68b3d893
ramesh konatala <konatalaramesh@gmail.com>
2016-02-29 15:55:04 +0530

    Search characters

--- regex.py
+++ regex.py
@@ -1,7 +1,7 @@
 import re
-phoneNumRegex=re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')# backslash is used to escape characters proceeding it
-mo=phoneNumRegex.search('My number is (412)-321-3232')
+phoneNumRegex=re.compile(r'Bat(man|mobile|copter|bat)')# backslash is used to escape characters proceeding it
+mo=phoneNumRegex.search('Batmobile lost a wheel')
 print mo.groups()
 print mo.group(1)
-print mo.group(2)
+print mo.group()
 print mo.group(0)
