2a58366b8e856e1e2c26f156319301849d32289e
ramesh konatala <konatalaramesh@gmail.com>
2016-02-29 16:03:48 +0530

    use of findall

--- regex.py
+++ regex.py
@@ -1,7 +1,7 @@
 import re
-phoneNumRegex=re.compile(r'(Ha){3}')# backslash is used to escape characters proceeding it
-mo=phoneNumRegex.search('you are HaHaHa')
+phoneNumRegex=re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')# backslash is used to escape characters proceeding it
+print phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
 #print mo.groups()
 #print mo.group(1)
-print mo.group()
+#print mo.group()
 #print mo.group(0)
