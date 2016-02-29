e4a118874ec27d33db96392a4611244d5394cfd0
ramesh konatala <konatalaramesh@gmail.com>
2016-02-29 16:00:21 +0530

    use of curly brackets

--- regex.py
+++ regex.py
@@ -1,7 +1,7 @@
 import re
-phoneNumRegex=re.compile(r'Bat(man|mobile|copter|bat)')# backslash is used to escape characters proceeding it
-mo=phoneNumRegex.search('Batmobile lost a wheel')
-print mo.groups()
-print mo.group(1)
+phoneNumRegex=re.compile(r'(Ha){3}')# backslash is used to escape characters proceeding it
+mo=phoneNumRegex.search('you are HaHaHa')
+#print mo.groups()
+#print mo.group(1)
 print mo.group()
-print mo.group(0)
+#print mo.group(0)
