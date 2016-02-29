db05faabb1cc655e7ac5523453f8671658f80a0b
ramesh konatala <konatalaramesh@gmail.com>
2016-02-29 15:52:40 +0530

    Backslash use

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
-phoneNumRegex=re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-mo=phoneNumRegex.search('My number is 412-321-3232')
+phoneNumRegex=re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')# backslash is used to escape characters proceeding it
+mo=phoneNumRegex.search('My number is (412)-321-3232')
 print mo.groups()
 print mo.group(1)
 print mo.group(2)
