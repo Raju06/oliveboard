f5a91bceeca67bd164e8f5499c7ea8b952e05a07
ramesh konatala <konatalaramesh@gmail.com>
2016-03-01 17:47:10 +0530

    temporary

--- calculate.py
+++ calculate.py
@@ -1,6 +1,2 @@
-from sympy import sin, limit,solve
-from sympy import Symbol
-x = Symbol('x')
-y = Symbol('y')
-print solve(x-((5**3)*20)/((125**(1/3))*((,x)
-print solve(y**2-4,y)
+input_value='0'+'2'
+print input_value
