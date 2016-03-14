5465e160efce4ffbbc6a2d63ad16c92722460b8c
rameshkonatala <konatalaramesh@gmail.com>
2016-03-14 16:04:16 +0530

    Submits login information in webpage automatically

--- learnings/webScraping/selenium_tut.py
+++ learnings/webScraping/selenium_tut.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 browser=webdriver.Firefox()
-browser.get('http://youtube.com')
-linkElem=browser.find_element_by_link_text('Trending')
-type(linkElem)
-linkElem.click()
+browser.get('http://accounts.google.com')
+emailElem=browser.find_element_by_id('Email')
+emailElem.send_keys('konatalaramesh@gmail.com')
+emailElem.submit()
+passwordElem=browser.find_element_by_id('Passwd-hidden')
+passwordElem.submit()
