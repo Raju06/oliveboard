0c926c46a949a7eec83eb5994c3614d2892df695
rameshkonatala <konatalaramesh@gmail.com>
2016-03-14 15:04:00 +0530

    working now

--- learnings/webScraping/lucky.py
+++ learnings/webScraping/lucky.py
@@ -1,9 +1,14 @@
 import requests,sys,webbrowser,bs4
 
 print('Googling....')    #display text while downloading the Google page
-res=requests.get('https://www.google.co.in/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q='+' '.join(sys.argv[1:]))
+res=requests.get('https://www.google.co.in/search?q='+' '.join(sys.argv[1:]))
 res.raise_for_status()
 soup=bs4.BeautifulSoup(res.text,"html.parser")
+
+with open('C:\Users\Olive\Desktop\oliveboard\learnings\webScraping\prettifyHTML.txt', 'w') as f:
+   for line in soup.prettify('utf-8', 'minimal'):
+      f.write(str(line))
+
 linkElems=soup.select('.r a')
 print linkElems
 numOpen=min(5,len(linkElems))
