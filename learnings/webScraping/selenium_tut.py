04001eb69230a527a38cf65836766c6cd99a929e
rameshkonatala <konatalaramesh@gmail.com>
2016-03-14 18:14:05 +0530

    Modified to use home and end keys

--- learnings/webScraping/selenium_tut.py
+++ learnings/webScraping/selenium_tut.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 browser=webdriver.Firefox()
-browser.get('http://accounts.google.com')
-emailElem=browser.find_element_by_id('Email')
-emailElem.send_keys('konatalaramesh@gmail.com')
-emailElem.submit()
-passwordElem=browser.find_element_by_id('Passwd-hidden')
-passwordElem.submit()
+browser.get('http://nostarch.com')
+htmlElem=browser.find_element_by_tag_name('html')
+htmlElem.send_keys(Keys.END) #scrolls to bottom
+htmlElem.send_keys(Keys.HOME) #scrolls to top
