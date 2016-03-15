b5bdd363a70c9d9312172465bf0ec473e7d7ba50
rameshkonatala <konatalaramesh@gmail.com>
2016-03-15 17:50:14 +0530

    modified to delete 1 stage known elements from unsolved list

--- project_language/language.py
+++ project_language/language.py
@@ -1,48 +1,53 @@
+from sets import Set
 from itertools import combinations
-def compare(i,j,left,right):
-	left_list=[]
-	right_list=[]
-	for i_item in left[i]:
-		for j_item in left[j]:
-			if i_item == j_item:
-				left_list.append(i_item)
 
-	for i_item in right[i]:
-		for j_item in right[j]:
-			if i_item == j_item:
-				right_list.append(i_item)
-	return left_list,right_list
+def compare(i,j,left,right):
+	
+	left_set_i=set(left[i])
+	left_set_j=set(left[j])
+	right_set_i=set(right[i])
+	right_set_j=set(right[j])
+
+	left_list_intersection=list(left_set_i & left_set_j)
+	right_list_intersection=list(right_set_i & right_set_j)
+	
+	left_list_subtract = list(left_set_i - left_set_j)
+	right_list_subtract = list(right_set_i - right_set_j)
+	return left_list_intersection,right_list_intersection,left_list_subtract,right_list_subtract
+
 
 def comparewhole(combinations_list,left,right):
-	left_list=[]
-	right_list=[]
+	solved_left_list=[]
+	solved_right_list=[]
+	unsolved_left_list=[]
+	unsolved_right_list=[]
 	for i in range(len(combinations_list)):
-		left_temp,right_temp=compare(combinations_list[i][0],combinations_list[i][1],left,right)
-		left_list.append(left_temp)
-		right_list.append(right_temp)
-	return left_list,right_list
+		left_intersection,right_intersection,left_subtract,right_subtract=compare(combinations_list[i][0],combinations_list[i][1],left,right)
+		if len(left_intersection)==1 and left_intersection[0] not in solved_left_list:	
+			solved_left_list.append(left_intersection[0])
+			solved_right_list.append(right_intersection[0])
+		if len(left_subtract)>1 and left_subtract not in unsolved_left_list:
+			unsolved_left_list.append(left_subtract)
+			unsolved_right_list.append(right_subtract) 
+	return solved_left_list,solved_right_list,unsolved_left_list,unsolved_right_list
 
-def separatesingle(left_list,right_list,left,right):
-	final_left_list=[]
-	final_right_list=[]
-	i=0
-	while i<(len(left_list)):
-		if len(left_list[i])==1:
-			if left_list[i][0] not in final_left_list:
-				final_left_list.append(left_list[i].pop())
-				final_right_list.append(right_list[i].pop())
-			else:
-				i+=1
-		else: 
-			if len(left_list[i])>1:
-				if left_list[i] not in left:	
-					left.append(left_list.pop(left_list.index(left_list[i])))
-					right.append(right_list.pop(right_list.index(right_list[i])))
-				else:
-					left_list.pop(left_list.index(left_list[i]))
-					right_list.pop(right_list.index(right_list[i]))
+def removeSingleItems(left_list_single,right_list_single,left_main,right_main):
+	left_list_unsolved=[]
+	right_list_unsolved=[]
+	for i in range(len(left_main)):
+		temp_i=list(set(left_main[i])-set(left_list_single))
+		temp_j=list(set(right_main[i])-set(right_list_single))
+		temp_i_unsolved=list(set(left_main[i])&set(left_list_single))
+		temp_j_unsolved=list(set(right_main[i])&set(right_list_single))
+		if len(temp_i)==1 and temp_i not in left_list_single:
+			left_list_single.append(temp_i[0])
+			right_list_single.append(temp_j[0])
+		if len(temp_i_unsolved)>1 and temp_i_unsolved not in left_list_unsolved:
+			left_list_unsolved.append(temp_i_unsolved)
+			right_list_unsolved.append(temp_j_unsolved) 
+	return left_list_single,right_list_single,left_list_unsolved,right_list_unsolved
 
-	return final_left_list,final_right_list
+
 
 
 
@@ -54,16 +59,18 @@ while temp!='0':
 	right.append(raw_input("Enter right list\n").split())
 	temp=raw_input("Enter 0 if list is done : ")
 
+
 combinations_list=map(list,combinations([i for i in range(len(left))],2))
-left_list,right_list=comparewhole(combinations_list,left,right)
-final_left_list,final_right_list=separatesingle(left_list,right_list,left,right)
+solved_left_list,solved_right_list,unsolved_left_list,unsolved_right_list=comparewhole(combinations_list,left,right)
 
 
-print "Main lists are : \n"
-for i in range(len(left)):
-	print "{} is {}".format(left[i],right[i]) 
+solved_left_list,solved_right_list,unsolved_left_list,unsolved_right_list=removeSingleItems(solved_left_list,solved_right_list,unsolved_left_list,unsolved_right_list)
+print "solved left list is \n",solved_left_list
+print "solved right list is \n",solved_right_list
+print "unsolved left list is \n",unsolved_left_list
+print "unsolved right list is \n",unsolved_right_list
 
-for i in range(len(final_left_list)):
-	print "{} is {}".format(final_left_list[i],final_right_list[i]) 
+for i in range(len(solved_left_list)):
+	print "{} is {}".format(solved_left_list[i],solved_right_list[i]) 
 #print "left list is {}".format(left_list)
 #print "right list is {}".format(right_list)
