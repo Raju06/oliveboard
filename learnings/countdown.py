0adcdd8577e5d69979b8ca2608277fba1065ef3d
rameshkonatala <konatalaramesh@gmail.com>
2016-03-15 18:09:11 +0530

    modified

--- learnings/countdown.py
+++ learnings/countdown.py
@@ -2,10 +2,10 @@
 # countdown.py - A simple countdown script.
 
 import time, subprocess
-timeLeft = 20
+timeLeft = 5
 while timeLeft > 0:
 	print(timeLeft,'\n')
 	time.sleep(1)
 	timeLeft = timeLeft - 1
 
-subprocess.Popen(['start', 'alarm.wav'], shell=True)
+subprocess.Popen(['start', 'Alarm01.wav'], shell=True)
