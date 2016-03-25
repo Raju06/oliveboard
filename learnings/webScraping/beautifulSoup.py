ddc8877604dce4738014c4b9de4e15a13d7282bf
rameshkonatala <konatalaramesh@gmail.com>
2016-03-25 16:27:38 +0530

    Will search and generates dictionary with items required

--- learnings/webScraping/beautifulSoup.py
+++ learnings/webScraping/beautifulSoup.py
@@ -1,19 +1,15 @@
 from bs4 import BeautifulSoup
 import json
 import urllib
-r=urllib.urlopen('http://www.aflcio.org/Legislation-and-Politics/Legislative-Alerts').read()
+r=urllib.urlopen('https://iimcat.ac.in/per/g01/pub/756/ASM/WebPortal/1/index.html?756@@1@@1').read()
 soup=BeautifulSoup(r,'lxml')
-letters=soup.find_all('div',class_="ec_statements")
+letters=soup.find_all('section',class_="home-content")
+print letters
 lobbying={}
 for element in letters:
+	print element.a.get_text()
 	lobbying[element.a.get_text()]={}
-prefix="www.aflcio.org"
 for element in letters:
-	date=element.find(id='legalert_date').get_text()
-	lobbying[element.a.get_text()]['link']=prefix+element.a['href']
-	lobbying[element.a.get_text()]['date']=date
+	lobbying[element.a.get_text()]=element.a['href']
 for item in lobbying.keys():
-	print item + ": " +"\n\t"+"link: "+lobbying[item]["link"]+"\n\t"+"date: "+lobbying[item]["date"]+"\n\n"
-
-with open("lobbying.json","w") as writeJSON:
-	json.dump(lobbying,writeJSON)	
+	print item + ": " +"\n\t"+"link: "+lobbying[item]+"\n\n"
