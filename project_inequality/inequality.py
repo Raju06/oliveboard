3da64b6738fe2dd9c452dc1eda01d940e04f85e4
rameshkonatala <konatalaramesh@gmail.com>
2016-03-25 18:41:00 +0530

    Will separate given expression into a 2D matrix

---
+++ project_inequality/inequality.py
@@ -0,0 +1,20 @@
+def generateMatrix():
+	matrix=[['.' for j in range(80)] for i in range(80)]
+	for i in range(len(a)):
+		#print ' '.join(a[i])
+		pass
+
+
+expression=raw_input('Enter the expression separated by , :').split(',')
+expression_list=[]
+for i in range(len(expression)):
+	expression_list.append(['','',''])
+	for j in range(len(expression[i])):
+		if expression[i][j] in ['>','<','=']:
+			expression_list[i][2]+=expression[i][j]
+		else:
+			expression_list[i]=expression[i][j]
+
+#for i in range(len(expression_list)):
+
+generateMatrix()
