96bce958f00d2a17804909e312e049ebbc237825
rameshkonatala <konatalaramesh@gmail.com>
2016-03-29 14:26:13 +0530

    Will separate operators and variables in expression

--- project_inequality/inequality.py
+++ project_inequality/inequality.py
@@ -4,18 +4,22 @@ def generateMatrix():
 		#print ' '.join(a[i])
 		pass
 
+def mergeExpression(expression_dict):
+
 
 expression=raw_input('Enter the expression separated by , :').split(',')
 print expression
-expression_list=[]
+expression_dict={}
+expression_dict['variables']=[]
+expression_dict['operators']=[]
 for i in range(len(expression)):
-	expression_list.append(['','',''])
+	expression_dict['variables'].append([])
+	expression_dict['operators'].append([])
 	for j in range(len(expression[i])):
 		if expression[i][j] in ['>','<','=']:
-			expression_list[i][2]+=expression[i][j]
+			expression_dict['operators'][len(expression_dict['operators'])-1].append(expression[i][j])
 		else:
-			expression_list[i]=expression[i][j]
+			expression_dict['variables'][len(expression_dict['variables'])-1].append(expression[i][j])
 
 #for i in range(len(expression_list)):
-
-generateMatrix()
+print expression_dict
