a9562a80a022869b0444cc4e32f67b4a4d71e8f9
rameshkonatala <konatalaramesh@gmail.com>
2016-03-29 17:53:05 +0530

    Will generate combined equations

--- project_inequality/inequality.py
+++ project_inequality/inequality.py
@@ -1,25 +1,113 @@
-def generateMatrix():
-	matrix=[['.' for j in range(80)] for i in range(80)]
-	for i in range(len(a)):
-		#print ' '.join(a[i])
-		pass
-
-def mergeExpression(expression_dict):
+from itertools import permutations
 
 
-expression=raw_input('Enter the expression separated by , :').split(',')
-print expression
-expression_dict={}
-expression_dict['variables']=[]
-expression_dict['operators']=[]
-for i in range(len(expression)):
-	expression_dict['variables'].append([])
-	expression_dict['operators'].append([])
-	for j in range(len(expression[i])):
-		if expression[i][j] in ['>','<','=']:
-			expression_dict['operators'][len(expression_dict['operators'])-1].append(expression[i][j])
+class expression:
+	def  __init__(self):
+		self.items=''
+
+	def isEmpty(self):
+		return self.items == ''
+
+	def addRight(self, item):
+		self.items+=item
+
+	def addLeft(self, item):
+		self.items=item+self.items
+	
+	def size(self):
+		return len(self.items)
+
+	def nothingToLeft(self,item):
+		alist=[]
+		for i in range(len(self.items)):
+			if self.items[i]==item:
+				alist.append(i)
+		if 0 in alist:
+			return True
 		else:
-			expression_dict['variables'][len(expression_dict['variables'])-1].append(expression[i][j])
+			return False
 
-#for i in range(len(expression_list)):
-print expression_dict
+
+	def nothingToRight(self,item):
+		alist=[]
+		for i in range(len(self.items)):
+			if self.items[i]==item:
+				alist.append(i)
+		if len(self.items)-1 in alist:
+			return True
+		else:
+			return False
+
+	def returnExpression(self):
+		return self.items
+
+def opposite(operator):
+	if operator == '*':
+		return '%'
+	elif operator == '@':
+		return '$'
+	elif operator == '#':
+		return '#'
+	elif operator == '$':
+		return '@'
+	elif operator == '%':
+		return '*'
+
+def expressionOpposite(exp):
+	expression=''
+	i=0
+	while i<len(exp)-2:
+		expression+=exp[len(exp)-i-1]+opposite(exp[len(exp)-i-2])
+		i+=2
+	return expression+exp[0]
+
+
+expression_list=raw_input('Enter the expression separated by comma(,) :').split(',')
+
+def main(expression_list):
+	def equation(expression_list):
+		expression_list=list(expression_list)
+		a=expression()
+		a.addRight(expression_list.pop(0))
+		for i in range(len(expression_list)):
+			if expression_list[i][0] in a.returnExpression():
+				if a.nothingToRight(expression_list[i][0]):
+					a.addRight(expression_list[i][1:])
+				elif a.nothingToLeft(expression_list[i][0]):
+					expression_list[i]=expression_list[i][:1]+opposite(expression_list[i][1])+expression_list[i][2:]
+					a.addLeft(expression_list[i][1:][::-1])
+				else:
+					pass
+			elif expression_list[i][2] in a.returnExpression():
+				if a.nothingToRight(expression_list[i][2]):
+					expression_list[i]=expression_list[i][:1]+opposite(expression_list[i][1])+expression_list[i][2:]
+					a.addRight(expression_list[i][:2][::-1])
+				elif a.nothingToLeft(expression_list[i][2]):
+					a.addLeft(expression_list[i][:2])
+				else:
+					pass
+		return a.returnExpression()
+
+	expression_list_new=[]
+	alist=[]
+	for c in permutations(expression_list):
+		expression_list_new.append(list(c))
+
+	for i in range(len(expression_list_new)):
+		temp=equation(expression_list_new[i])
+		if temp not in alist:
+			found=False
+			for j in range(len(alist)):
+				if temp in alist[j]:
+					found=True
+					break
+			if not found:
+				alist.append(temp)
+						
+	finalList=[]					
+	for i in range(len(alist)):
+		if expressionOpposite(alist[i]) not in finalList:
+			finalList.append(alist[i])
+	return finalList
+
+print main(expression_list)
