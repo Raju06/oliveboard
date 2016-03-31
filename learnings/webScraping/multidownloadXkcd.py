673a2f2a70e37c14253f51c9aadf6c4f54db708a
ramesh konatala <ramesh konatala>
2016-04-01 01:42:20 +0530

    multidownload videos from other websites

--- learnings/webScraping/multidownloadXkcd.py
+++ learnings/webScraping/multidownloadXkcd.py
@@ -1,8 +1,43 @@
 #! python3
    # multidownloadXkcd.py - Downloads XKCD comics using multiple threads.
+from __future__ import unicode_literals
+import youtube_dl
+import requests,sys,webbrowser,bs4,re
+from pytube import YouTube
 
+# not necessary, just for demo purposes.
+from pprint import pprint
 import requests, os, bs4, threading
-os.makedirs('xkcd') # store comics in ./xkcd
+ # store comics in ./xkcd
+
+def main(item):
+	ydl_opts = {}
+
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		ydl.download([item])
+
+
+    #display text while downloading the Google page
+def mainCode(numberStart,numberStop):
+	for number in range(numberStart,numberStop):
+		if number==0:
+			k=''
+		else:
+			k=str('/&p={}'.format(number))
+		item=str('http://www.xvideos.com/?k=indian+college{}'.format(k))
+		print 'Downloading {}...'.format(str('http://www.xvideos.com/?k=indian+college{}'.format(k)))
+		res=requests.get(item)
+		res.raise_for_status()
+		soup=bs4.BeautifulSoup(res.text,"html.parser")
+		linkElems=soup.select('.mozaique a')
+		print len(linkElems)
+		URLlist=[]
+		for i in range(len(linkElems)):
+			URLlist.append(str('http://www.xvideos.com'+linkElems[i]['href']))
+		for i in range(len(URLlist)):
+			main(URLlist[i])
+			print 'downloading ',URLlist[i]
+
 
 def downloadXkcd(startComic, endComic):
   for urlNumber in range(startComic, endComic):
@@ -28,10 +63,12 @@ def downloadXkcd(startComic, endComic):
       imageFile.close()
 
 downloadThreads=[] # a list of all Thread objects
-for i in range(0,100,10): # loops 14 times,creates 14 threads
-  downloadThread=threading.Thread(target=downloadXkcd,args=(i,i+9))
-  downloadThreads.append(downloadThread)
-  downloadThread.start()
+k=0
+for number in range(0,10,2):
+	
+	downloadThread=threading.Thread(target=mainCode,args=(number,number+1))
+	downloadThreads.append(downloadThread)
+	downloadThread.start()
 
 for downloadThread in downloadThreads:
     downloadThread.join()
